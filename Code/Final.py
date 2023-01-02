30d34263b7f2bac310f53b892f2f5f3b509390de
Eldar Shmueli <eldar05050@gmail.com>
2023-01-03 00:59:27 +0200

    Fixed everything

--- Code/Final.py
+++ Code/Final.py
@@ -1,8 +1,9 @@
-
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import Select
 import bs4
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -12,64 +13,69 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 global url
 url = "https://www.nba.com/stats/leaders"
-html_stats_url = 'stats.html'
 
-def load_soup_elment(html_name):
-    html_file = open(html_name)
-    soup = BeautifulSoup(html_file, 'html.parser')
+
+
+def load_soup_elment():
+    #Loading Soup object and return it
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
     return soup
 
+def create_df_dict():
+    #Creating a Dictionary for the Data Frame base on the Columns names and return it
+    ID, Name, Team, GP, Minute, Points, FGM, FGA, FG_P = ([] for i in range(9))
+    Three_PM, Three_PA, Three_Per, FTM, FTA, FT_P = ([] for i in range(6))
+    OREB, DREB, REB, AST, STL, BLK, TOV, EFF, BIO = ([] for i in range(9))
+    dict = {
+        'ID':ID, 'Name':Name, 'Team':Team, 'GP':GP, 'Minute':Minute,
+        'Points':Points, 'FGM':FGM, 'FGA':FGA, 'FG_P':FG_P, '3PM':Three_PM,
+        '3PA':Three_PA, '3P%':Three_Per,'FTM':FTM, 'FTA':FTA, 'FT%':FT_P, 'OREB':OREB,
+        'DREB':DREB, 'REB':REB, 'AST':AST, 'STL':STL, 'BLK':BLK, 'TOV':TOV, 'EFF':EFF, 'BIO':BIO
+    }
+    return dict
+
 def bioInfo(playerUrl):
-    driver.get(playerUrl)
+    #Input - Player page URL
+    #Crawl to the url and return the BIO section(otherwise return non)
+    driver.get('https://www.nba.com' + playerUrl)
     try:
         driver.find_element(By.LINK_TEXT, "Bio").click()
-        print(driver.current_url)
         bio=driver.find_element(By.CLASS_NAME, "PlayerBio_player_bio__kIsc_").text
-        print(bio)
     except :
         bio = "non"
     return bio
 
-def getting_data():
-    driver.get(url)
-    ID, Name, Team, GP, Minute, Points, FGM, FGA, FG_P = ([] for i in range(9))
-    Three_PM, Three_PA, Three_Per, FTM, FTA, FT_P = ([] for i in range(6))
-    OREB, DREB, REB, AST, STL, BLK, TOV, EFF, BIO = ([] for i in range(9))
-    soup = load_soup_elment(html_stats_url)
-    rows = soup.find_all('tr')
-    rows.pop(0)
-    for row in rows:
-        cells = row.find_all('td')
-        ID.append(cells[0].text.strip())
-        Name.append(cells[1].text.strip())
-        Team.append(cells[2].text.strip())
-        GP.append(cells[3].text.strip())
-        Minute.append(cells[4].text.strip())
-        Points.append(cells[5].text.strip())
-        FGM.append(cells[6].text.strip())
-        FGA.append(cells[7].text.strip())
-        FG_P.append(cells[8].text.strip())
-        Three_PM.append(cells[9].text.strip())
-        Three_PA.append(cells[10].text.strip())
-        Three_Per.append(cells[11].text.strip())
-        FTM.append(cells[12].text.strip())
-        FTA.append(cells[13].text.strip())
-        FT_P.append(cells[14].text.strip())
-        OREB.append(cells[15].text.strip())
-        DREB.append(cells[16].text.strip())
-        REB.append(cells[17].text.strip())
-        AST.append(cells[18].text.strip())
-        STL.append(cells[19].text.strip())
-        BLK.append(cells[20].text.strip())
-        TOV.append(cells[21].text.strip())
-        EFF.append(cells[22].text.strip())
-        BIO.append(bioInfo(cells[1].find('a')['href']))
-        
 
-    df = pd.DataFrame({'ID':ID, 'Name':Name, 'Team':Team, 'GP':GP, 'Minute':Minute,
-    'Points':Points, 'FGM':FGM, 'FGA':FGA, 'FG_P':FG_P, '3PM':Three_PM, '3PA':Three_PA, '3P%':Three_Per,
-    'FTM':FTM, 'FTA':FTA, 'FT%':FT_P, 'OREB':OREB, 'DREB':DREB, 'REB':REB, 'AST':AST, 'STL':STL, 'BLK':BLK, 'TOV':TOV, 'EFF':EFF, 'BIO':BIO})
-    time.sleep(8)
+def getting_data():
+    #Creating the Data Frame, crawl to each NBA Season and save the Stats Table to DF
+    driver.get(url)
+    dict = create_df_dict()
+    df = pd.DataFrame(dict)
+    for season_num in range(10):
+        season = '20' + str(22 - season_num) + '-' + str(23 - season_num)
+        season_select = Select(driver.find_element(By.CLASS_NAME, 'DropDown_select__4pIg9'))
+        season_select.select_by_visible_text(season)
+        time.sleep(1)
+        soup = load_soup_elment()
+        page_drop = Select(driver.find_element(By.XPATH, '//*[@id="__next"]/div[2]/div[2]/div[3]/section[2]/div/div[2]/div[2]/div[1]/div[3]/div/label/div/select'))
+        page_drop.select_by_visible_text("All")
+        rows = soup.find_all('tr')
+        rows.pop(0)
+        for row in rows:
+            cells = row.find_all('td')
+            row_num = len(df)
+            player_row = []
+            for index, value in enumerate(dict.values()):
+                if(index != 23):
+                    player_row.append(cells[index].text.strip())
+                else:
+                    player_row.append(bioInfo(cells[1].find('a')['href']))
+            df.loc[row_num] = player_row
+        df.to_csv('Data.csv')
+
+        driver.get(url)
+    df.to_csv('Data.csv')
     driver.quit()
     
 getting_data()
