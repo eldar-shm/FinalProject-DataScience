2384a60b787ac31210c60c27d4639c33b57c30fb
Eldar Shmueli <eldar05050@gmail.com>
2023-01-09 22:05:38 +0200

    Add Func to crewl the All-Star Players names and export it to CSV

--- Code/Final.py
+++ Code/Final.py
@@ -12,8 +12,7 @@ import time
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 global url
-url = "https://www.nba.com/stats/leaders"
-
+statsUrl = "https://www.nba.com/stats/leaders"
 
 
 def load_soup_elment():
@@ -41,42 +40,68 @@ def bioInfo(playerUrl):
     driver.get('https://www.nba.com' + playerUrl)
     try:
         driver.find_element(By.LINK_TEXT, "Bio").click()
-        bio=driver.find_element(By.CLASS_NAME, "PlayerBio_player_bio__kIsc_").text
+        bio = driver.find_element(By.CLASS_NAME, "PlayerBio_player_bio__kIsc_").text
     except :
         bio = "non"
     return bio
 
+def get_all_star_data():
+    players = []
+    for seasonNum in range(10):
+        season = str(2022 - seasonNum)
+        allStarUrl = 'https://www.basketball-reference.com/allstar/NBA_' + str(season) + '.html'
+        driver.get(allStarUrl)
+        soup = load_soup_elment()
+        time.sleep(2)
+        seasonPlayers = []
+        playersTags = soup.find_all('th', attrs={'scope':'row', 'class':'left', 'data-stat':'player'})
+        for tag in playersTags:
+            try:
+                Name = tag.find('a').text
+                if Name not in players:
+                    players.append(Name)
+            except:
+                continue
+
+    df = pd.DataFrame({'Player Name':players})
+    df.to_csv('All-Star Players.csv')
+
+    
+
+
+
 
 def getting_data():
     #Creating the Data Frame, crawl to each NBA Season and save the Stats Table to DF
-    driver.get(url)
-    dict = create_df_dict()
-    df = pd.DataFrame(dict)
-    for season_num in range(10):
-        season = '20' + str(22 - season_num) + '-' + str(23 - season_num)
+    driver.get(statsUrl)
+    dictToDF = create_df_dict()
+    df = pd.DataFrame(dictToDF)
+    for seasonNum in range(10):
+        season = '20' + str(22 - seasonNum) + '-' + str(23 - seasonNum)
         season_select = Select(driver.find_element(By.CLASS_NAME, 'DropDown_select__4pIg9'))
         season_select.select_by_visible_text(season)
         time.sleep(2)
-        page_drop = Select(driver.find_element(By.XPATH, '//*[@id="__next"]/div[2]/div[2]/div[3]/section[2]/div/div[2]/div[2]/div[1]/div[3]/div/label/div/select'))
-        page_drop.select_by_visible_text("All")
+        pageDrop = Select(driver.find_element(By.XPATH, '//*[@id="__next"]/div[2]/div[2]/div[3]/section[2]/div/div[2]/div[2]/div[1]/div[3]/div/label/div/select'))
+        pageDrop.select_by_visible_text("All")
         time.sleep(2)
         soup = load_soup_elment()
         rows = soup.find_all('tr')
         rows.pop(0)
         for row in rows:
             cells = row.find_all('td')
-            row_num = len(df)
-            player_row = []
-            for index, value in enumerate(dict.values()):
+            rowNum = len(df)
+            playerRow = []
+            for index, value in enumerate(dictToDF.values()):
                 if(index != 23):
-                    player_row.append(cells[index].text.strip())
+                    playerRow.append(cells[index].text.strip())
                 else:
-                    player_row.append(bioInfo(cells[1].find('a')['href']))
-            df.loc[row_num] = player_row
+                    playerRow.append(bioInfo(cells[1].find('a')['href']))
+            df.loc[rowNum] = playerRow
         df.to_csv('Data.csv')
 
-        driver.get(url)
+        driver.get(statsUrl)
     df.to_csv('Data.csv')
     driver.quit()
     
-getting_data()
+#getting_data()
+get_all_star_data()
